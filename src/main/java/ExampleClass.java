39ed06c5aa987ab5856503247c4c4bdb305f576f
Vasilevich_Y <magicisnear@bk.ru>
2022-02-17 12:59:51 +0300

    latest

--- src/main/java/ExampleClass.java
+++ src/main/java/ExampleClass.java
@@ -10,8 +10,6 @@ import java.util.stream.Collectors;
 
 public class ExampleClass {
 
-    private static final String GET_Users_ENDPOINT_URL = "http://91.241.64.178:7081/api/users";
-    private static final String GET_User_ENDPOINT_URL = "http://91.241.64.178:7081/api/users/{id}";
     private static final String CREATE_User_ENDPOINT_URL = "http://91.241.64.178:7081/api/users";
     private static final String UPDATE_User_ENDPOINT_URL = "http://91.241.64.178:7081/api/users";
     private static final String DELETE_User_ENDPOINT_URL = "http://91.241.64.178:7081/api/users";
@@ -20,32 +18,14 @@ public class ExampleClass {
 
     public static void main(String[] args) {
         String cookies = getUsers();
-        System.out.println(cookies);
-
-        User user = new User (3L, "James", "Brown", (byte) 25);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Cookie",cookies);
-        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpEntity<User> requestBody = new HttpEntity<>(user,headers);
-
-        ResponseEntity<String> response = restTemplate
-                .exchange(CREATE_User_ENDPOINT_URL, HttpMethod.POST, requestBody, String.class);
-
-        System.out.println(response.getBody());
-
-//        createUser(cookies);
-//        updateUser(cookies);
-//        deleteUser(cookies);
-   }
+        createUser(cookies);
+        updateUser(cookies);
+       deleteUser(cookies);
+    }
 
     private static String getUsers() {
         HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+        headers.setAccept(Arrays.asList(new MediaType[]{MediaType.APPLICATION_JSON}));
         RestTemplate template = new RestTemplate();
         ResponseEntity<String> forEntity = template.getForEntity("http://91.241.64.178:7081/api/users", String.class);
         forEntity.getHeaders().get("Set-Cookie").stream().forEach(System.out::println);
@@ -53,45 +33,47 @@ public class ExampleClass {
     }
 
     private static void createUser(String cookies) {
-        User user = new User (3L, "James", "Brown", (byte) 25);
+        User user = new User(3L, "James", "Brown", (byte) 25);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", cookies);
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
         RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<User> entity = new HttpEntity<>(user);
-        ResponseEntity<String> result
-                = restTemplate.postForEntity(CREATE_User_ENDPOINT_URL, entity, String.class);
-        System.out.println(result);
+        HttpEntity<User> requestBody = new HttpEntity<>(user, headers);
+        ResponseEntity<String> response = restTemplate
+                .exchange(CREATE_User_ENDPOINT_URL, HttpMethod.POST, requestBody, String.class);
+        System.out.println(response.getBody());
     }
 
     private static void updateUser(String cookies) {
         Long userId = 3l;
         User updateUser = new User(userId, "Thomas", "Shelby", (byte) 25);
+
         HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", cookies);
         headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<User> requestBody = new HttpEntity<>(updateUser, headers);
-        restTemplate.exchange(UPDATE_User_ENDPOINT_URL, HttpMethod.PUT, requestBody, Void.class);
-
-        String resourceUrl = GET_User_ENDPOINT_URL + "/" + userId;
-        User user =  restTemplate.getForObject(resourceUrl, User.class);
-        if (user != null) {
-            System.out.println("(Client side) User after update: ");
-            System.out.println("User: " + user.getName() + " - " + user.getLastName());
-        }
-
+        ResponseEntity<String> response =
+                restTemplate.exchange(UPDATE_User_ENDPOINT_URL, HttpMethod.PUT, requestBody, String.class);
+        System.out.println(response.getBody());
     }
 
     private static void deleteUser(String cookies) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cookie", cookies);
+
         RestTemplate restTemplate = new RestTemplate();
-        String resourceUrl = DELETE_User_ENDPOINT_URL+ "/" + 3;
-        restTemplate.delete(resourceUrl);
-
-        User user =  restTemplate.getForObject(resourceUrl, User.class);
-
-        if (user != null) {
-            System.out.println("(Client side) User after update: ");
-            System.out.println("User: " + user.getName() + " - " + user.getLastName());
-        } else {
-            System.out.println("User not found!");
-        }
+        HttpEntity<String> requestBody = new HttpEntity<>(null, headers);
+        String resourceUrl = DELETE_User_ENDPOINT_URL + "/" + 3;
+        ResponseEntity<String> response =
+                restTemplate.exchange(resourceUrl, HttpMethod.DELETE, requestBody, String.class);
+        System.out.println(response.getBody());
     }
 
 }
